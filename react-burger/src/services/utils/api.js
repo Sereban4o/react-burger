f522a243128cf5f6587629a78847687999d0a0e5
Sereban4o <106669883+Sereban4o@users.noreply.github.com>
2023-11-16 11:44:12 +0300

    Update api.js

--- react-burger/src/services/utils/api.js
+++ react-burger/src/services/utils/api.js
@@ -1,5 +1,7 @@
 export const getRequest = async (url, post = {}) => {
   const response = await fetch(url, post);
-  const data = await response.json();
-  return data;
+  if (response.ok) {
+    return response.json();
+  }
+  return Promise.reject(`Ошибка ${response.status}`);
 };
